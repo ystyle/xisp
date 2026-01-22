3bb7a8463ecc8808acb35271af32d05bbd6d83d6
YSTYLE <lxy5266@live.com>
2026-01-23 00:14:30 +0800

    fix(thread-first): 修复管道操作符支持高阶函数
    - 移除 builtin_higher_order.cj 中的 map/filter/reduce NativeFunc 定义
    - 修复 threadFirstList 构造调用的方式，正确处理参数顺序
    - 修改 evalMap/evalFilter/evalReduce 检测值列表，避免重复求值
    - 修正 modern_syntax.lisp 中的期望值（50 而不是 54）
    - 将 modern_syntax.lisp 改为带输出的交互式示例
    - 所有 100 个单元测试通过

--- examples/modern_syntax.lisp
+++ examples/modern_syntax.lisp
@@ -4,100 +4,106 @@
 ;; Xisp 支持现代化的 Lisp 语法特性
 
 ;; ==================== 1. 向量字面量 [] ====================
-;; 创建向量（内部转换为列表）
-[1 2 3]
-;; => (1.000000 2.000000 3.000000)
+(println "=== 1. 向量字面量 [] ===")
+(println "创建向量 [1 2 3]:")
+(define v1 [1 2 3])
+(println v1)
 
-;; 向量可以包含任意类型
-[1 "hello" #t]
-;; => (1.000000 "hello" true)
+(println "\n向量可以包含任意类型 [1 \"hello\" #t]:")
+(define v2 [1 "hello" #t])
+(println v2)
 
-;; 嵌套向量
-[[1 2] [3 4]]
-;; => ((1.000000 2.000000) (3.000000 4.000000))
+(println "\n嵌套向量 [[1 2] [3 4]]:")
+(define v3 [[1 2] [3 4]])
+(println v3)
 
 ;; ==================== 2. 哈希映射字面量 {} ====================
-;; 创建关联列表
-{:name "Alice" :age 30 :city "Beijing"}
-;; => ((:name . "Alice") (:age . 30.000000) (:city . "Beijing"))
+(println "\n=== 2. 哈希映射字面量 {} ===")
+(println "创建关联列表 {:name \"Alice\" :age 30 :city \"Beijing\"}:")
+(define m1 {:name "Alice" :age 30 :city "Beijing"})
+(println m1)
 
-;; 嵌套哈希映射
-{:user {:name "Bob" :id 123} :role :admin}
-;; => ((:user . (:name . "Bob" :id . 123.000000)) (:role . :admin))
+(println "\n嵌套哈希映射 {:user {:name \"Bob\" :id 123} :role :admin}:")
+(define m2 {:user {:name "Bob" :id 123} :role :admin})
+(println m2)
 
 ;; ==================== 3. 哈希集合字面量 #{} ====================
-;; 创建集合（内部转换为列表）
-#{1 2 3 4 5}
-;; => (1.000000 2.000000 3.000000 4.000000 5.000000)
+(println "\n=== 3. 哈希集合字面量 #{} ===")
+(println "创建集合 #{1 2 3 4 5}:")
+(define s1 #{1 2 3 4 5})
+(println s1)
 
-;; 符号集合
-#{:red :green :blue}
-;; => (:red :green :blue)
+(println "\n符号集合 #{:red :green :blue}:")
+(define s2 #{:red :green :blue})
+(println s2)
 
 ;; ==================== 4. 字符串插值 #"" ====================
-;; 在字符串中插入变量和表达式
-#"Value is: #{x}"
-;; 插入多个值
-#"x=#{x}, y=#{y}, sum=#{+ x y}"
+(println "\n=== 4. 字符串插值 #\"\" ===")
+(define x 10)
+(define y 20)
+(println "简单插值 #\"Value is: #{x}\":")
+(println #"Value is: #{x}")
 
-;; 复杂表达式插值
-#"Result: #{* x x}"  ; x 的平方
+(println "\n插入多个值 #\"x=#{x}, y=#{y}, sum=#{+ x y}\":")
+(println #"x=#{x}, y=#{y}, sum=#{+ x y}")
+
+(println "\n复杂表达式插值 #\"Result: #{* x x}\" (x 的平方):")
+(println #"Result: #{* x x}")
 
 ;; ==================== 5. 解构绑定 (let) ====================
-;; 使用现代向量语法进行解构绑定
-(let [[x y z] (quote (1 2 3))]
-  (str "x=" x ", y=" y ", z=" z))
-;; => "x=1, y=2, z=3"
+(println "\n=== 5. 解构绑定 (let) ===")
+(println "解构 [x y z]:")
+(define r1 (let [[x y z] (quote (1 2 3))]
+  (str "x=" x ", y=" y ", z=" z)))
+(println r1)
 
-;; 使用 & 收集剩余元素
-(let [[x y & rest] (quote (1 2 3 4 5))]
-  (str "x=" x ", y=" y ", rest=" rest))
-;; => "x=1, y=2, rest=(3 4 5)"
+(println "\n使用 & 收集剩余元素 [x y & rest]:")
+(define r2 (let [[x y & rest] (quote (1 2 3 4 5))]
+  (str "x=" x ", y=" y ", rest=" rest)))
+(println r2)
 
-;; 嵌套解构
-(let [[[a b] [c d]] (quote ((1 2) (3 4)))]
-  (str "a=" a ", b=" b ", c=" c ", d=" d))
-;; => "a=1, b=2, c=3, d=4"
+(println "\n嵌套解构 [[a b] [c d]]:")
+(define r3 (let [[[a b] [c d]] (quote ((1 2) (3 4)))]
+  (str "a=" a ", b=" b ", c=" c ", d=" d)))
+(println r3)
 
 ;; ==================== 6. 管道操作符 -> ====================
-;; 线程宏：将前一个结果作为后一个函数的第一个参数
-(-> 10
-    (+ 5)
-    (* 2))
-;; => 30.000000
+(println "\n=== 6. 管道操作符 -> ===")
+(println "简单管道 (-> 10 (+ 5) (* 2)):")
+(define p1 (-> 10 (+ 5) (* 2)))
+(println p1)
 
-;; 复杂管道操作
-(-> (quote (1 2 3 4 5))
+(println "\n复杂管道操作 (map square, filter > 5, reduce +):")
+(define p2 (-> (quote (1 2 3 4 5))
   (map (lambda (x) (* x x)))
   (filter (lambda (x) (> x 5)))
-  (reduce + 0))
-;; => 54.000000 (16 + 25)
+  (reduce + 0)))
+(println p2)
+(println "期望: 50 (9 + 16 + 25)")
 
 ;; ==================== 7. 高阶函数 lambda 支持 ====================
-;; map、filter、reduce 都支持 lambda
+(println "\n=== 7. 高阶函数 lambda 支持 ===")
+(println "map lambda 平方:")
+(define map1 (map (lambda (x) (* x x)) (quote (1 2 3 4 5))))
+(println map1)
 
-;; 使用 lambda 映射
-(map (lambda (x) (* x x))
-     (quote (1 2 3 4 5)))
-;; => (1.000000 4.000000 9.000000 16.000000 25.000000)
+(println "\nfilter lambda 过滤 > 3:")
+(define filter1 (filter (lambda (x) (> x 3)) (quote (1 2 3 4 5))))
+(println filter1)
 
-;; 使用 lambda 过滤
-(filter (lambda (x) (> x 3))
-        (quote (1 2 3 4 5)))
-;; => (4.000000 5.000000)
-
-;; 使用 lambda 归约
-(reduce (lambda (acc x) (+ acc x))
-        0
-        (quote (1 2 3 4 5)))
-;; => 15.000000
+(println "\nreduce lambda 求和:")
+(define reduce1 (reduce (lambda (acc x) (+ acc x)) 0 (quote (1 2 3 4 5))))
+(println reduce1)
 
 ;; ==================== 8. 组合使用 ====================
-;; 管道 + lambda + 字面量
+(println "\n=== 8. 组合使用 ===")
 (define numbers [1 2 3 4 5])
-
-(-> numbers
+(println "管道 + lambda + 字面量:")
+(define combo (-> numbers
     (map (lambda (n) (* n n)))
     (filter (lambda (n) (> n 5)))
-    (reduce + 0))
-;; => 54.000000
+    (reduce + 0)))
+(println combo)
+(println "期望: 50 (9 + 16 + 25)")
+
+(println "\n=== 所有测试完成 ===")
